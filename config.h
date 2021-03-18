03f0ce42774f3e8e98c3159501a97542190f2555
ClementPhan <>
2021-03-18 22:40:17 +0100

    Make config reflect physical wiring

--- config.h
+++ config.h
@@ -33,9 +33,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 12
-#define COL_EXPANDED { false, false, false, false, false, false, true, true, true, true, true, true }
+#define COL_EXPANDED { true, true, true, true, true, true, false, false, false, false, false, false }
 #define MATRIX_ONBOARD_ROW_PINS { F0, F1, F4, F5, F6, F7 }
-#define MATRIX_ONBOARD_COL_PINS { B1, B2, B3, D2, D3, C6, 0, 0, 0, 0, 0, 0 }
+#define MATRIX_ONBOARD_COL_PINS { 0, 0, 0, 0, 0, 0, B1, B2, B3, D2, D3, C6 }
 #define EXPANDER_COL_REGISTER GPIOA
 #define EXPANDER_ROW_REGISTER GPIOB
 #define MATRIX_EXPANDER_COL_PINS {0, 1, 2, 3, 4, 5}
